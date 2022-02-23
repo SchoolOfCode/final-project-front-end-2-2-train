1d344c8a17e0b8819dfacec51faa7d57b9efd7ed
AnnaMFedorenko <94460566+AnnaMFedorenko@users.noreply.github.com>
2022-02-23 14:19:53 +0000

    useForm hook done

--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -1,38 +1,84 @@
-import React from "react";
+import { ColorLensOutlined } from "@mui/icons-material";
+import { React, useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import style from "./Form.module.css";
-//const API_URL = "https://room-22-train.herokuapp.com";
+const API_URL = "https://room-22-train.herokuapp.com";
 
 export default function Form({ opened }) {
+   //Styling for the menu icon
    const styleAdd = opened
       ? `${style.sidebarContainer}`
       : `${style.sidebarContainer} ${style.sidebarHidden}`;
+   const [obj, setObj] = useState({});
+   //Using useForm hook to add validation to the form in line with HTML standards.
    const {
       register,
       handleSubmit,
       watch,
       formState: { errors },
    } = useForm();
-   const onSubmit = (data) => console.log(data);
+   const onSubmit = (data) => setObj(data);
+   const [media, setMedia] = useState([]);
+   const [error, setError] = useState("");
 
    console.log(watch("example")); // watch input value by passing the name of it
+   // The POST request.
 
+   useEffect(() => {
+      if (Object.keys(obj).length === 0) {
+         return;
+      } else {
+         async function getMedia() {
+            try {
+               const response = await fetch(`${API_URL}/media`, {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(obj),
+               });
+               const result = await response.json();
+               if (result.success === true) {
+                  console.log(result);
+                  setMedia(result.payload);
+                  setError("");
+               } else {
+                  console.log(response);
+                  setError("Fetch didn't work :(");
+               }
+            } catch (err) {
+               console.log(err);
+               setError(err.message);
+            }
+         }
+         getMedia();
+      }
+   }, [obj]);
+   //The callback function "register" passes the input into the useForm Hook.
+   //"Required" adds validation to inputted data.
    return (
       <div className={styleAdd}>
          <form
             className={style.formContainer}
             onSubmit={handleSubmit(onSubmit)}>
             <input
+               type="file"
                placeholder="Image"
                {...register("aws_key", { required: true })}
             />
             <input
                placeholder="Title"
-               {...register("media_title", { required: true })}
+               {...register("media_title", {
+                  required: true,
+                  minLength: 1,
+                  maxLength: 40,
+               })}
             />
             <input
                placeholder="Location"
-               {...register("location", { required: true })}
+               {...register("location", {
+                  required: true,
+                  minLength: 1,
+                  maxLength: 40,
+               })}
             />
             <input
                placeholder="Date"
@@ -42,9 +88,13 @@ export default function Form({ opened }) {
             <input
                className={style.formContainerTextarea}
                placeholder="Note"
-               {...register("media_descr", { required: true })}
+               {...register("media_descr", {
+                  required: true,
+                  minLength: 1,
+                  maxLength: 80,
+               })}
             />
-            {/* errors will return when field validation fails  */}
+            {/* Errors will return when field validation fails  */}
             {errors.exampleRequired && <span>This field is required</span>}
 
             <input className={style.formContainerButton} type="submit" />
