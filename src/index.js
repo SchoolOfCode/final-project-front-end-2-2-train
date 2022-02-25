06a2452ba6afe316272d0b39f6d71a7e5d0f7e02
Graham <graycanny@hotmail.com>
2022-02-25 10:36:38 +0000

    auth and routes working

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
       <Auth0Provider
          domain="dev-zstf-6ko.us.auth0.com"
          clientId="u1uyOmpEhNMThAh66lmghT1xqnKv6hgT"
-         redirectUri={window.location.origin}>
+         redirectUri={`http://localhost:3000/app`}>
          <Router>
             <Routes>
                <Route path="/" element={<Landing />}></Route>
