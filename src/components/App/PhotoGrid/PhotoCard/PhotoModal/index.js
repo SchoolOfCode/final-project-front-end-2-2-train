f42760591bd421b7f4995ec3e14136489f9f5980
Graham <graycanny@hotmail.com>
2022-02-28 15:07:17 +0000

    Comments deleted

--- src/components/App/PhotoGrid/PhotoCard/PhotoModal/index.js
+++ src/components/App/PhotoGrid/PhotoCard/PhotoModal/index.js
@@ -1,21 +1,7 @@
-import { DataObject } from "@mui/icons-material";
 import React from "react";
 import style from "./PhotoModal.module.css";
 
 function PhotoModal({ photo, note }) {
-   //  // Get the modal
-   //  var modal = document.getElementById("myModal");
-
-   //  // Get the <span> element that closes the modal
-   //  var span = document.getElementsByClassName("close")[0];
-
-   //  // When the user clicks anywhere outside of the modal, close it
-   //  window.onclick = function (event) {
-   //     if (event.target == modal) {
-   //        modal.style.display = "none";
-   //     }
-   //  };
-
    return (
       <div>
          <div className={style.modal}>
