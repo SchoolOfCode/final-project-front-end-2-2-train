9bca77a10ad8519c204f61c84ee58c09633ce9cb
Tahmid Hussain <TahmidHussain@Tahmids-Mac-mini.local>
2022-02-28 10:40:30 +0000

    css changes

--- src/components/App/PhotoGrid/PhotoCard/index.js
+++ src/components/App/PhotoGrid/PhotoCard/index.js
@@ -5,27 +5,36 @@ function PhotoCard({ key, dataObj, delFunc }) {
    // css to allow only a certain amount of char for the notes
    // returning data populated by the DummyData
    return (
-      < div  >
-         <div className={style.photoContainer} >
-
-            <img className={style.polaroidFrame} src={require("../../../../img/photo-frame.png")} alt='Polaroid Frame' />
-            <div className={style.photoDiv}>
-            <img className={style.photo} src={dataObj.aws_key} alt={key} />
+      <div className={style.flipcard}>
+         <div className={style.flipcardContainer}>
+            <div className={style.flipcardFront}>
+               <img
+                  className={style.polaroidFrame}
+                  src={require("../../../../img/photo-frame.png")}
+                  alt="Polaroid Frame"
+               />
+               <div className={style.photoDiv}>
+                  {" "}
+                  <img
+                     className={style.photo}
+                     src={dataObj.aws_key}
+                     alt={key}
+                  />{" "}
+               </div>
             </div>
-
+            <div className={style.flipcardBack}>
+               <button onClick={() => delFunc(dataObj.id)}>
+                  Delete photograph #{dataObj.id}
+               </button>
+               <h1>{dataObj.media_title}</h1>
+               <h2>
+                  {dataObj.location} - {dataObj.date}
+               </h2>
+               <h3>{dataObj.media_descr}</h3>
+            </div>
          </div>
-         </div>
+      </div>
    );
 }
 
 export default PhotoCard;
-               // <div className={style.photoBack}>
-               // <button onClick={() => delFunc(dataObj.id)}>
-               //    Delete photograph #{dataObj.id}
-               // </button>
-               // <h1>{dataObj.media_title}</h1>
-               // <h2>
-               //    {dataObj.location} - {dataObj.date}
-               // </h2>
-               // <h3>{dataObj.media_descr}</h3>
-               // </div>
