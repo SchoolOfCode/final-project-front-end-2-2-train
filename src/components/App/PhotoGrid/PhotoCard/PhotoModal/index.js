35366a02983dc647094b43e438504c682f3f1a11
Graham <graycanny@hotmail.com>
2022-02-28 15:05:54 +0000

    map changed

--- src/components/App/PhotoGrid/PhotoCard/PhotoModal/index.js
+++ src/components/App/PhotoGrid/PhotoCard/PhotoModal/index.js
@@ -1,49 +1,31 @@
-import React from 'react'
+import { DataObject } from "@mui/icons-material";
+import React from "react";
+import style from "./PhotoModal.module.css";
 
-function PhotoModal() {
-    // Get the modal
-var modal = document.getElementById("myModal");
+function PhotoModal({ photo, note }) {
+   //  // Get the modal
+   //  var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+   //  // Get the <span> element that closes the modal
+   //  var span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+   //  // When the user clicks anywhere outside of the modal, close it
+   //  window.onclick = function (event) {
+   //     if (event.target == modal) {
+   //        modal.style.display = "none";
+   //     }
+   //  };
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-  return (
-    <div>
-    {/* <!-- Trigger/Open The Modal --> */}
-    <button id="myBtn">Open Modal</button>
-    
-    {/* <!-- The Modal --> */}
-    <div id="myModal" class="modal">
-    
-      {/* <!-- Modal content --> */}
-      <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Some text in the Modal..</p>
+   return (
+      <div>
+         <div className={style.modal}>
+            <img src={photo} alt="Modal Pic" />
+            <div className={style.modalContent}>
+               <p>{note}</p>
+            </div>
+         </div>
       </div>
-    
-    </div>
-    </div>
-  )
+   );
 }
 
-export default PhotoModal
+export default PhotoModal;
--- src/components/App/PhotoGrid/PhotoCard/index.js
+++ src/components/App/PhotoGrid/PhotoCard/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./photocard.module.css";
+import PhotoModal from "./PhotoModal";
 
 function PhotoCard({ key, dataObj, delFunc }) {
    // css to allow only a certain amount of char for the notes
    // returning data populated by the DummyData
+   const [modal, setModal] = useState(false);
+
    return (
       <div className={style.flipcard}>
-         <div className={style.flipcardContainer}>
+         <div
+            className={style.flipcardContainer}
+            onClick={() => setModal(true)}>
             <div className={style.flipcardFront}>
                <img
                   className={style.polaroidFrame}
@@ -23,17 +28,24 @@ function PhotoCard({ key, dataObj, delFunc }) {
                </div>
             </div>
             <div className={style.flipcardBack}>
-            <div className={style.textHolder} >
-               <h1 className={style.photoTitle} >{dataObj.media_title}</h1>
-               <h2 className={style.photoTitle} >
-                  {dataObj.location} <br/> {dataObj.date}
-               </h2>
+               <div className={style.textHolder}>
+                  <h1 className={style.photoTitle}>{dataObj.media_title}</h1>
+                  <h2 className={style.photoTitle}>
+                     {dataObj.location} <br /> {dataObj.date}
+                  </h2>
                </div>
-               <p onClick={() => delFunc(dataObj.id)} className={style.deleteBtn} >
+               <p
+                  onClick={() => delFunc(dataObj.id)}
+                  className={style.deleteBtn}>
                   X
                </p>
             </div>
          </div>
+         {modal ? (
+            <PhotoModal photo={dataObj.aws_key} notes={dataObj.note} />
+         ) : (
+            <></>
+         )}
       </div>
    );
 }
