a41caeed588017899c824b834329c392bfdeebb9
Graham <graycanny@hotmail.com>
2022-02-28 12:10:13 +0000

    Moved folders around

--- src/components/App/Navbar/index.js
+++ src/components/App/Navbar/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import style from './Navbar.module.css'
 import Form from "../Form";
-import MenuIcon from "../MenuIcon";
-import LogoutButton from "../LogoutBtn";
+import MenuIcon from "./MenuIcon";
+import LogoutButton from "./LogoutBtn";
 
 
 function Navbar({opened, setOpened}) {
