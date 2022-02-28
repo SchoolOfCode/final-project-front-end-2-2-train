1fd50ea89ff0e41b321686289465e7c5c817fa25
Graham <graycanny@hotmail.com>
2022-02-28 09:52:01 +0000

    modal created

--- src/components/App/PhotoGrid/PhotoCard/index.js
+++ src/components/App/PhotoGrid/PhotoCard/index.js
@@ -5,22 +5,27 @@ function PhotoCard({ key, dataObj, delFunc }) {
    // css to allow only a certain amount of char for the notes
    // returning data populated by the DummyData
    return (
-      <div className={style.container}>
-         <div className={style.photoContainer}>
-            <img className={style.photoFront} src={dataObj.aws_key} alt={key} />
-            <div className={style.photoBack}>
-               <button onClick={() => delFunc(dataObj.id)}>
-                  Delete photograph #{dataObj.id}
-               </button>
-               <h1>{dataObj.media_title}</h1>
-               <h2>
-                  {dataObj.location} - {dataObj.date}
-               </h2>
-               <h3>{dataObj.media_descr}</h3>
+      < div  >
+         <div className={style.photoContainer} >
+
+            <img className={style.polaroidFrame} src={require("../../../../img/photo-frame.png")} alt='Polaroid Frame' />
+            <div className={style.photoDiv}>
+            <img className={style.photo} src={dataObj.aws_key} alt={key} />
             </div>
+
          </div>
-      </div>
+         </div>
    );
 }
 
 export default PhotoCard;
+               // <div className={style.photoBack}>
+               // <button onClick={() => delFunc(dataObj.id)}>
+               //    Delete photograph #{dataObj.id}
+               // </button>
+               // <h1>{dataObj.media_title}</h1>
+               // <h2>
+               //    {dataObj.location} - {dataObj.date}
+               // </h2>
+               // <h3>{dataObj.media_descr}</h3>
+               // </div>
