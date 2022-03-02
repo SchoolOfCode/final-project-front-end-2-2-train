763c70238dee8e436a2e20cf16574b257b8a29f3
Nadja <92398598+NadjaGrothe@users.noreply.github.com>
2022-03-02 16:32:43 +0000

    FormDisaply test working

--- src/components/App/Form/Form.test.js
+++ src/components/App/Form/Form.test.js
@@ -1,70 +1,19 @@
 import React from "react";
-import {
-   render,
-   screen,
-   waitFor,
-   within,
-   cleanup,
-} from "@testing-library/react";
-import user from "@testing-library/user-event";
-import Form from "./index";
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Form from "./index.js";
 
-// afterEach(cleanup);
+describe("FormDisplay", () => {
+   it("should render all the Form input fields", () => {
+      render(<Form />);
 
-// it("should be displayed", () => {
-//    render(<Form />);
-//    const FormElement = screen.getByRole("");
-//    expect(asFragment(<Form />)).toMatchSnapshot();
-// });
-afterEach(cleanup);
-describe("Form", () => {
-   const onSubmit = jest.fn();
-
-   beforeEach(() => {
-      onSubmit.mockClear();
-      render(<Form onSubmit={onSubmit} />);
-   });
-
-   it("onSubmit is called when all fields pass validation", async () => {
-      user.type(getImage(), "Image");
-      user.type(getTitle(), "Holiday");
-
-      user.type(getLocation(), "London");
-
-      user.type(getDate(), "1201202");
-
-      user.type(getNote(), "lovely hot holiday");
-
-      clickSubmitButton();
-
-      await waitFor(() => {
-         expect(onSubmit).toHaveBeenCalledWith({});
-      });
-
-      expect(onSubmit).toHaveBeenCalledTimes(1);
+      expect(screen.getByPlaceholderText(/image/i)).toBeInTheDocument();
+      expect(screen.getByPlaceholderText(/title/i)).toBeInTheDocument();
+      expect(screen.getByPlaceholderText(/location/i)).toBeInTheDocument();
+      expect(screen.getByPlaceholderText(/date/i)).toBeInTheDocument();
+      expect(screen.getByPlaceholderText(/note/i)).toBeInTheDocument();
+      expect(
+         screen.getByRole("button", { name: /submit/i })
+      ).toBeInTheDocument();
    });
 });
-
-function getImage() {
-   return screen.getByPlaceholderText("textbox", { name: /image/i });
-}
-
-function getTitle() {
-   return screen.getByRole("textbox", { name: /media_title/i });
-}
-
-function getLocation() {
-   return screen.getByRole("textbox", { name: /location/i });
-}
-
-function getDate() {
-   return screen.getByPlaceholderText("textbox", { name: /date/i });
-}
-
-function getNote() {
-   return screen.getByRole("textbox", { name: /media_descr/i });
-}
-
-function clickSubmitButton() {
-   user.click(screen.getByRole("button", { name: /submit/i }));
-}
