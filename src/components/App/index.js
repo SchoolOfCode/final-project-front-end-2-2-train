9ccd02f2daaf92b0593b447417632fe653a7b129
Graham <graycanny@hotmail.com>
2022-03-07 09:57:01 +0000

    URL added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,16 +7,14 @@ import Form from "./Form";
 import { useAuth0 } from "@auth0/auth0-react";
 import MarkerMap from "../Map";
 
-
 const API_URL = "http://localhost:5500";
-// const API_URL = "https://room-22-train.herokuapp.com";
+// const API_URL = "https://git.heroku.com/gray2-2.git";
 
 function App() {
    // gets the user information after authentication
    const { user, isLoading } = useAuth0();
 
    if (isLoading) <p>Loading...</p>;
-
 
    // Sets the style of the sidebar to show it
    const [form, setForm] = useState(false);
@@ -31,7 +29,6 @@ function App() {
          const email = user.email;
          console.log(email);
          try {
-
             const response = await fetch(`${API_URL}/users/${user.email}`);
             const newData = await response.json();
             console.log("THIS IS THE DATA IN THE MOUNTED USEEFFECT", newData);
@@ -57,7 +54,7 @@ function App() {
       console.log(formLocation);
    }, [formLocation]);
 
-
+   return (
       <div className={style.app}>
          <Navbar className={style.navbar} />
          <div className={style.mapContainer}>
