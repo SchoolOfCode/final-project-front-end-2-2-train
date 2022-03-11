36141fcb80bf6fcdab62d33a83fceb89f70dac71
Graham <graycanny@hotmail.com>
2022-03-11 09:45:42 +0000

    profile pic added

--- src/components/App/Navbar/index.js
+++ src/components/App/Navbar/index.js
@@ -18,7 +18,7 @@ function Navbar({ profilePic, setMapLoc }) {
                />
                <img
                   className={style.profile}
-                  src={profilePic}
+                  src={require("../../../img/profile-pic.png")}
                   alt="Users profile"
                />
             </div>
@@ -45,9 +45,8 @@ function Navbar({ profilePic, setMapLoc }) {
                      return `${item.tip}`;
                   })}
                />
-            </div>
-
                <GeoSearch setMapLoc={setMapLoc} />
+            </div>
 
             <LogoutButton />
          </div>
